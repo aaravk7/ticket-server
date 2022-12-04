465af3404fec265a47832d28dbeb5e14196262a9
Aarav <aaravkhokhar@yahoo.in>
2022-12-05 02:49:12 +0530

    Another fix

--- src/routes/admin/event.ts
+++ src/routes/admin/event.ts
@@ -108,11 +108,10 @@ router.post(
         .status(400)
         .json({ error: "Poster can only be a jpeg or png image" });
 
-    const extension = poster.name.split(".").at(-1);
     const posterName = path.join(
       cwd(),
       "dist/public/posters",
-      Date.now().toString() + "." + extension
+      Date.now().toString() + poster.name
     );
     try {
       await poster.mv(posterName);
