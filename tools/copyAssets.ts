bac4954e78d1e9e33887ae9c0923d57c88a64543
Aarav <aaravkhokhar@yahoo.in>
2022-12-05 03:16:12 +0530

    Fix in build command

--- tools/copyAssets.ts
+++ tools/copyAssets.ts
@@ -1,5 +1,5 @@
-import { cp, mkdir } from "shelljs";
+import { cp, mkdir, mv } from "shelljs";
 
 cp("-R", ["src/views"], "dist/");
 mkdir("dist/public");
-mkdir("dist/public/posters");
+mv("-f", "./public", "dist");
